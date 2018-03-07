7542f4313cf2b7c0b638c325481bc79e3374765c
jongHyeon yeo <jonghyeon.rw@gmail.com>
2018-03-07 14:05:42 +0900

    add - print yeojonghyeon name 201424492

--- git/src/git/jedo66.java
+++ git/src/git/jedo66.java
@@ -4,5 +4,6 @@ public class jedo66 {
 	  public static void main(String args[]) {
 		    System.out.println("hello world");
 		    System.out.println("yujooyeong");
+		    System.out.println("yeojonghyeon");
 	}
 }
